3d0d727a28d3bc5ac5b2cc2670f3df6c7abc2e1e
Deepesh Nehra <nehradeepesh009@gmail.com>
2026-07-02 23:09:26 +0530

    Configure Node project structure for Vercel functions

--- api/config.js
+++ api/config.js
@@ -1,8 +1,8 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   res.status(200).json({
     CONN_TOKEN: process.env.CONN_TOKEN || null,
     DB_NAME: process.env.DB_NAME || null,
     REL_NAME: process.env.REL_NAME || null,
     BASE_URL: process.env.BASE_URL || null
   });
-}
+};
